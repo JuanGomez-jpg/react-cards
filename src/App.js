4650be7a6171fb0f40f0c7d95573801af97f7b11
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2022-07-28 19:13:06 -0500

    Component to class base component

--- src/App.js
+++ src/App.js
@@ -1,102 +1,93 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import Card from './modules/Card';
 import './CSS/App.css'
-import styled from 'styled-components';
+import Button from './element/Button';
+import {ThemeProvider} from 'styled-components';
 
-const Button = styled.button`
-  background-color: ${props => props.length > 2 ? '#4CAF50': props.length < 2 ? 'red': 'black'};
-  border: none;
-  color: ${props => props.length <= 1 ? 'black' : 'white'};
-  font-weight: ${props => props.length <= 1 ? 'bold' : 'normal'};
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-`;
+const theme = {
+  primary: '#4CAF50',
+  mango: 'yellow'
+};
 
+class App extends Component {
 
-function App() {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      cards: [
+        {
+          id: 1,
+          name: 'El cacas',
+          tittle: 'The Shit',
+          avatar: 'https://i.pravatar.cc/150?img=8'
+        },
+        {
+          id: 2,
+          name: 'La cochina',
+          tittle: 'The motherfucker',
+          avatar: 'https://i.pravatar.cc/150?img=49'
+        },
+        {
+          id: 3,
+          name: 'La legal',
+          tittle: 'The bitch',
+          avatar: 'https://i.pravatar.cc/150?img=30'
+        }
+      ],
+      showCard: true
+    }
+  }
 
-  const buttonChange =
-  (
-    <div className="buttons">
-      <button type="button" className="button button3">
-      change Name
-      </button>
-    </div>
-  );
-
-  const [showCard, setShowCard] = useState(true);
-  const toggleShowCard = () => setShowCard(!showCard);
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards];
+  toggleShowCard = () => this.setState({showCard: !this.state.showCard});
+  deleteCardHandler = (cardIndex) => {
+    const cards_copy = [...this.state.cards];
     cards_copy.splice(cardIndex, 1);
-    console.log('cards_copy', cards_copy);
-    console.log('cards', cards);
-    setCards(cards_copy);
+    this.setState({cards:cards_copy});
   };
 
-  const changeNameHandler = (event, id) => {
+  changeNameHandler = (event, id) => {
     //1. Which card is going to be deleted
-    const cardIndex = cards.findIndex(card => card.id == id);
+    const cardIndex = this.state.cards.findIndex(card => card.id === id);
     //2. Make a copy of the cards
-    const cards_copy = [...cards];
+    const cards_copy = [...this.state.cards];
     //3. Change the name of the specific card
     cards_copy[cardIndex].name = event.target.value;
     //4. Set the cards with the latest version of card copy
-    setCards(cards_copy);
+    this.setState({cards:cards_copy});
   };
 
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      name: 'El cacas',
-      tittle: 'The Shit',
-      avatar: 'https://i.pravatar.cc/150?img=8'
-    },
-    {
-      id: 2,
-      name: 'La cochina',
-      tittle: 'The motherfucker',
-      avatar: 'https://i.pravatar.cc/150?img=49'
-    },
-    {
-      id: 3,
-      name: 'La legal',
-      tittle: 'The bitch',
-      avatar: 'https://i.pravatar.cc/150?img=30'
-    }
-  ]);
+  render()
+  {
+    const classes = ['button'];
+    if (this.state.cards.length < 3 ) classes.push('black')
+    if (this.state.cards.length < 2 ) classes.push('red')
 
-  const classes = ['button'];
-  if (cards.length < 3 ) classes.push('black')
-  if (cards.length < 2 ) classes.push('red')
+    const cardLayout = this.state.showCard && (
+      this.state.cards.map((card, index) =>
+        <Card
+          avatar ={card.avatar}
+          name = {card.name}
+          tittle = {card.tittle}
+          key = {card.id}
+          onDelete = {() => this.deleteCardHandler(index)}
+          onChangeName = {(event) => this.changeNameHandler(event, card.id)}
+           />
+        )
+    )
 
-  const cardLayout = showCard && (
-    cards.map((card, index) =>
-      <Card
-        avatar ={card.avatar}
-        name = {card.name}
-        tittle = {card.tittle}
-        key = {card.id}
-        onDelete = {() => deleteCardHandler(index)}
-        onChangeName = {(event) => changeNameHandler(event, card.id)}
-         />
-      )
-  )
-
-  return (
-    <div className="App">
-      <Button length={cards.length} >Toggle</Button>
-      <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle Show/Hide Card</button>
-      {cardLayout}
+    return (
+      <ThemeProvider theme= {theme}>
+        <div className="App">
+          <Button color="mango" length={this.state.cards.length} onClick={this.toggleShowCard} >Toggle</Button>
+          <button className={classes.join(' ')} onClick={this.toggleShowCard}>Toggle Show/Hide Card</button>
+          {cardLayout}
+          </div>
+      </ThemeProvider>
+    );
+  }
 
 
-      </div>
-  );
 
 }
 
--- src/modules/Card.js
+++ src/modules/Card.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../CSS/CardCSS.css';
 
-const Card = props => {
-  return (
-    <div className="card">
-      <img src={props.avatar} className="image" alt="Avatar" />
-        <div className="separator" ></div>
-          <div className="container">
-            <h4>{props.name}</h4>
-            <p>{props.tittle}</p>
-            <p><button type="button" className="button button-red" onClick={props.onDelete}>Delete</button></p>
-            <input type="text" onChange={props.onChangeName} value={props.name} />
-            <div>{props.children}</div>
-        </div>
-    </div>
-  )
+class Card extends Component {
+  render ()
+  {
+    return (
+      <div className="card">
+        <img src={this.props.avatar} className="image" alt="Avatar" />
+          <div className="separator" ></div>
+            <div className="container">
+              <h4>{this.props.name}</h4>
+              <p>{this.props.tittle}</p>
+              <p><button type="button" className="button button-red" onClick={this.props.onDelete}>Delete</button></p>
+              <input type="text" onChange={this.props.onChangeName} value={this.props.name} />
+              <div>{this.props.children}</div>
+          </div>
+      </div>
+    )
+  }
 }
 
 
