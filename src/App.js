d713f8b5f0387b578fcc0bda163bec7ef9b21d39
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2022-07-30 16:41:32 -0500

    Axios implemented

--- src/App.js
+++ src/App.js
@@ -1,77 +1,59 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from './modules/Card';
 import './CSS/App.css'
 import Button from './element/Button';
 import {ThemeProvider} from 'styled-components';
+import axios from 'axios';
 
 const theme = {
   primary: '#4CAF50',
   mango: 'yellow'
 };
 
-class App extends Component {
+function App () {
 
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      cards: [
-        {
-          id: 1,
-          name: 'El cacas',
-          tittle: 'The Shit',
-          avatar: 'https://i.pravatar.cc/150?img=8'
-        },
-        {
-          id: 2,
-          name: 'La cochina',
-          tittle: 'The motherfucker',
-          avatar: 'https://i.pravatar.cc/150?img=49'
-        },
-        {
-          id: 3,
-          name: 'La legal',
-          tittle: 'The bitch',
-          avatar: 'https://i.pravatar.cc/150?img=30'
-        }
-      ],
-      showCard: true
-    }
-  }
+  const [showCard, setShowCard] = useState(true);
+  const [cards, setCards] = useState([]);
 
-  toggleShowCard = () => this.setState({showCard: !this.state.showCard});
-  deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...this.state.cards];
+  useEffect (() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res =>{
+      console.log(res.data);
+      setCards(res.data);
+    })
+  }, [])
+
+  const toggleShowCard = () => setShowCard(!showCard);
+  const deleteCardHandler = (cardIndex) => {
+    const cards_copy = [...cards]
     cards_copy.splice(cardIndex, 1);
-    this.setState({cards:cards_copy});
+    setCards(cards_copy);
   };
 
-  changeNameHandler = (event, id) => {
+  const changeNameHandler = (event, id) => {
     //1. Which card is going to be deleted
-    const cardIndex = this.state.cards.findIndex(card => card.id === id);
+    const cardIndex = cards.findIndex(card => card.id === id);
     //2. Make a copy of the cards
-    const cards_copy = [...this.state.cards];
+    const cards_copy = [...cards];
     //3. Change the name of the specific card
     cards_copy[cardIndex].name = event.target.value;
     //4. Set the cards with the latest version of card copy
-    this.setState({cards:cards_copy});
+    setCards(cards_copy);
   };
 
-  render()
-  {
-    const classes = ['button'];
-    if (this.state.cards.length < 3 ) classes.push('black')
-    if (this.state.cards.length < 2 ) classes.push('red')
 
-    const cardLayout = this.state.showCard && (
-      this.state.cards.map((card, index) =>
+    const classes = ['button'];
+    if (cards.length < 3 ) classes.push('black')
+    if (cards.length < 2 ) classes.push('red')
+
+    const cardLayout = showCard && (
+      cards.map((card, index) =>
         <Card
-          avatar ={card.avatar}
           name = {card.name}
-          tittle = {card.tittle}
+          phone = {card.phone}
           key = {card.id}
-          onDelete = {() => this.deleteCardHandler(index)}
-          onChangeName = {(event) => this.changeNameHandler(event, card.id)}
+          onDelete = {() => deleteCardHandler(index)}
+          onChangeName = {(event) => changeNameHandler(event, card.id)}
            />
         )
     )
@@ -79,15 +61,12 @@ class App extends Component {
     return (
       <ThemeProvider theme= {theme}>
         <div className="App">
-          <Button color="mango" length={this.state.cards.length} onClick={this.toggleShowCard} >Toggle</Button>
-          <button className={classes.join(' ')} onClick={this.toggleShowCard}>Toggle Show/Hide Card</button>
+          <Button color="mango" length={cards.length} onClick={toggleShowCard} >Toggle</Button>
+          <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle Show/Hide Card</button>
           {cardLayout}
           </div>
       </ThemeProvider>
     );
-  }
-
-
 
 }
 
--- src/modules/Card.js
+++ src/modules/Card.js
@@ -1,23 +1,19 @@
 import React, {Component} from 'react';
 import '../CSS/CardCSS.css';
 
-class Card extends Component {
-  render ()
-  {
+const Card = props => {
     return (
       <div className="card">
-        <img src={this.props.avatar} className="image" alt="Avatar" />
           <div className="separator" ></div>
             <div className="container">
-              <h4>{this.props.name}</h4>
-              <p>{this.props.tittle}</p>
-              <p><button type="button" className="button button-red" onClick={this.props.onDelete}>Delete</button></p>
-              <input type="text" onChange={this.props.onChangeName} value={this.props.name} />
-              <div>{this.props.children}</div>
+              <h4>{props.name}</h4>
+              <p>{props.phone}</p>
+              <p><button type="button" className="button button-red" onClick={props.onDelete}>Delete</button></p>
+              <input type="text" onChange={props.onChangeName} value={props.name} />
+              <div>{props.children}</div>
           </div>
       </div>
     )
-  }
 }
 
 
